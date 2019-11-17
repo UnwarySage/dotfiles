3588b1f791f8fb1d6a634c34f5c09463def8cc41
unwarysage <shawx538@morris.umn.edu>
2019-11-17 13:06:54 -0600

    Add an auto-watcher to lein

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,7 +3,9 @@
   ;;zprint style code formatting
     [[lein-zprint "0.5.3"]
   ;;cljfmt style code formatting
-     [lein-cljfmt "0.6.5"]]
+     [lein-cljfmt "0.6.5"]
+  ;;a task-re-runner
+     [lein-auto "0.1.3"]]
 
   :dependencies
     ;;use kondo linting 
